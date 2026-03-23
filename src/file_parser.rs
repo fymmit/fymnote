1635ef25ed248ba6566059c98e29b5e6d3fa398c
fymmit <blomqvist.timo@gmail.com>
2026-03-23 18:24:58 +0200

    Add a test

--- src/file_parser.rs
+++ src/file_parser.rs
@@ -50,7 +50,54 @@ fn find_notes_from_file_content(file_content: String) -> Vec<Note> {
 
 #[cfg(test)]
 mod tests {
+    use crate::{file_parser::find_notes_from_file_content, note::Note, timestamp::Timestamp};
 
     #[test]
-    fn finds_notes_from_file_content() {}
+    fn finds_notes_from_file_content() {
+        let file_content = String::from(
+            "\
+## 2026-03-22
+
+# 08.14
+test content
+
+# 08.26
+foo bar
+
+# 08.43
+fizz buzz",
+        );
+
+        let expected_notes: Vec<Note> = vec![
+            Note {
+                timestamp: Timestamp {
+                    date: String::from("2026-03-22"),
+                    time: String::from("08.14"),
+                },
+                content: String::from("test content"),
+            },
+            Note {
+                timestamp: Timestamp {
+                    date: String::from("2026-03-22"),
+                    time: String::from("08.26"),
+                },
+                content: String::from("foo bar"),
+            },
+            Note {
+                timestamp: Timestamp {
+                    date: String::from("2026-03-22"),
+                    time: String::from("08.43"),
+                },
+                content: String::from("fizz buzz"),
+            },
+        ];
+
+        let result_notes = find_notes_from_file_content(file_content);
+
+        for (i, item) in expected_notes.iter().enumerate() {
+            let expected = &expected_notes[i];
+            let result = &result_notes[i];
+            assert_eq!(format!("{expected}"), format!("{result}"));
+        }
+    }
 }
